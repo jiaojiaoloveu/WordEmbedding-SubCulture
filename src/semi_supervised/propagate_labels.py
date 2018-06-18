b14e651c249e244279ae7332c5a907cec9142053
Yuwei Jiao <jiaoywloveyou@gmail.com>
2018-06-17 21:24:43 -0400

    refactor & bugs

--- src/semi_supervised/propagate_labels.py
+++ src/semi_supervised/propagate_labels.py
@@ -9,26 +9,26 @@ from labels import Configs
 from gensim.models import KeyedVectors
 
 
+word_dataset_base = '../result/semi-supervised'
+
+
 def load_word_vectors(model_path):
     word_vectors = KeyedVectors.load_word2vec_format(model_path, binary=True)
     return word_vectors
 
 
 def mean_absolute_error(real_label, predict_label):
-    print(real_label.shape)
-    print(predict_label.shape)
     assert real_label.shape == predict_label.shape
     (token_size, feature_size) = real_label.shape
 
     mask = np.any(real_label, axis=1)
-    error_mat = (real_label - predict_label) * np.reshape(mask, (token_size, 1))
+    error_mat = np.absolute(real_label - predict_label) * np.reshape(mask, (token_size, 1))
     mae = np.sum(error_mat, axis=0) / np.sum(mask)
     print(mae)
     return mae
 
 
-def log_data(token_words, seed_words, eval_words):
-    word_dataset_base = '../result/semi-supervised'
+def log_data(token_words, seed_words, eval_words, word_matrix):
     os.makedirs(word_dataset_base, exist_ok=True)
     with open(os.path.join(word_dataset_base, 'token'), 'w') as fp:
         json.dump(token_words, fp)
@@ -36,15 +36,21 @@ def log_data(token_words, seed_words, eval_words):
         json.dump(seed_words, fp)
     with open(os.path.join(word_dataset_base, 'eval'), 'w') as fp:
         json.dump(eval_words, fp)
+    np.save(os.path.join(word_dataset_base, 'matrix'), word_matrix)
 
 
-def main():
-    ap = argparse.ArgumentParser("semi-supervised training using graph")
-    ap.add_argument('--train', type=int, required=False, default=5000)
-    ap.add_argument('--seed', type=int, required=False, default=50)
-    ap.add_argument('--eval', type=int, required=False, default=500)
-    ap.add_argument('--threshold', type=float, required=False, default=2.5)
-    args = vars(ap.parse_args())
+def reload_data():
+    with open(os.path.join(word_dataset_base, 'token'), 'w') as fp:
+        token_words = json.load(fp)
+    with open(os.path.join(word_dataset_base, 'seed'), 'w') as fp:
+        seed_words = json.dump(fp)
+    with open(os.path.join(word_dataset_base, 'eval'), 'w') as fp:
+        eval_words = json.dump(fp)
+    word_matrix = np.load(os.path.join(word_dataset_base, 'matrix'))
+    return token_words, seed_words, eval_words, word_matrix
+
+
+def generate():
     token_num = args.get('train')
     seed_num = args.get('seed')
     eval_num = args.get('eval')
@@ -73,8 +79,6 @@ def main():
     token_words = list(token_words)
     token_num = len(token_words)
 
-    log_data(token_words, seed_words, eval_words)
-
     weight_matrix = np.zeros((token_num, token_num), dtype=np.double)
 
     for ind in range(0, token_num - 1):
@@ -84,6 +88,12 @@ def main():
         weight_matrix[ind, ind + 1:] = 2 - google_news_model.distances(token_words[ind], token_words[ind + 1:])
 
     del google_news_model
+    log_data(token_words, seed_words, eval_words, weight_matrix)
+
+
+def train():
+    token_words, seed_words, eval_words, weight_matrix = reload_data()
+    token_num = len(token_words)
 
     weight_matrix = weight_matrix + weight_matrix.transpose()
     degree_matrix = np.eye(token_num) * np.sum(weight_matrix, axis=1)
@@ -104,13 +114,23 @@ def main():
                                eval_words[word][LabelSpace.P],
                                eval_words[word][LabelSpace.A]]
 
+    mean_absolute_error(eval_label, token_label)
     original_token_label = np.array(token_label)
     for it in range(0, Configs.iterations):
-        print('iteration # %s / %s' % (it, Configs.iterations))
+        print('iteration #%s/%s' % (it, Configs.iterations))
         transient_token_label = np.matmul(laplacian_matrix, token_label)
         token_label = Configs.alpha * transient_token_label + (1 - Configs.alpha) * original_token_label
         mean_absolute_error(eval_label, token_label)
 
 
 if __name__ == '__main__':
-    main()
+    ap = argparse.ArgumentParser("semi-supervised training using graph")
+    ap.add_argument('--train', type=int, required=False, default=5000)
+    ap.add_argument('--seed', type=int, required=False, default=50)
+    ap.add_argument('--eval', type=int, required=False, default=500)
+    ap.add_argument('--threshold', type=float, required=False, default=2.5)
+    ap.add_argument('--generate', type=int, required=False, default=0)
+    args = vars(ap.parse_args())
+    if args.get("generate") == 0:
+        generate()
+    train()
