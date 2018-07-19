69b708746902bbc96f6370598377e543202c3104
Yuwei Jiao <jiaoywloveyou@gmail.com>
2018-07-19 15:41:36 -0400

    typo

--- src/epa_expansion/compare.py
+++ src/epa_expansion/compare.py
@@ -4,9 +4,9 @@ tokens = ['male', 'female', 'adult', 'mom', 'father']
 
 
 if __name__ == '__main__':
-    with open('../../result/epa_expasion/nn_result_github_all', 'w') as fp:
+    with open('../result/epa_expansion/nn_result_github_all', 'w') as fp:
         github = json.load(fp)
-    with open('../../result/epa_expasion/nn_result_twitter_all', 'w') as fp:
+    with open('../result/epa_expansion/nn_result_twitter_all', 'w') as fp:
         twitter = json.load(fp)
     for token in tokens:
         print('%s %s %s' % (token, github[token], twitter[token]))
