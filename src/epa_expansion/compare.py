1bfe32dd72a6ab0e15f67e7c98b140591190b5c4
Yuwei Jiao <jiaoywloveyou@gmail.com>
2018-07-20 19:31:28 -0400

    compare result

--- src/epa_expansion/compare.py
+++ src/epa_expansion/compare.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 tokens = ['male', 'female', 'adult', 'mom', 'father']
 
@@ -12,3 +13,22 @@ if __name__ == '__main__':
         warriner = json.load(fp)
     for token in tokens:
         print('%s %s %s %s' % (token, github[token], twitter[token], warriner[token]))
+
+    common_tokens = set(github.keys()) & set(twitter.keys())
+    dis_e, dis_p, dis_a, dis_ave = {}, {}, {}, {}
+    for token in common_tokens:
+        diff = list(map(abs, github[token] - twitter[token]))
+        dis_e[token], dis_p[token], dis_a[token] = diff[0], diff[1], diff[2]
+        dis_ave[token] = np.mean(diff).item()
+    dis_e = sorted(dis_e, key=dis_e.get, reverse=True)
+    dis_p = sorted(dis_p, key=dis_p.get, reverse=True)
+    dis_a = sorted(dis_a, key=dis_a.get, reverse=True)
+    dis_ave = sorted(dis_ave, key=dis_ave.get, reverse=True)
+    with open('../result/epa_expansion/comparison_e') as fp:
+        json.dump(dis_e, fp)
+    with open('../result/epa_expansion/comparison_p') as fp:
+        json.dump(dis_p, fp)
+    with open('../result/epa_expansion/comparison_a') as fp:
+        json.dump(dis_a, fp)
+    with open('../result/epa_expansion/comparison_ave') as fp:
+        json.dump(dis_ave, fp)
