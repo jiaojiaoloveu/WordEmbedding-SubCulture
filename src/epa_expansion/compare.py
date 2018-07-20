700d131a42cd69f641b64d7e965a4e6b57407dd1
Yuwei Jiao <jiaoywloveyou@gmail.com>
2018-07-19 20:55:26 -0400

    compare

--- src/epa_expansion/compare.py
+++ src/epa_expansion/compare.py
@@ -8,5 +8,7 @@ if __name__ == '__main__':
         github = json.load(fp)
     with open('../result/epa_expansion/nn_result_twitter_all', 'r') as fp:
         twitter = json.load(fp)
+    with open('../data/epa/Ratings_Warriner_et_al_epa', 'r') as fp:
+        warriner = json.load(fp)
     for token in tokens:
-        print('%s %s %s' % (token, github[token], twitter[token]))
+        print('%s %s %s %s' % (token, github[token], twitter[token], warriner[token]))
