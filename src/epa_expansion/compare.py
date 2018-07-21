93b5b3e43ca4fb59a474be63a7330dfc9c9fce9d
Yuwei Jiao <jiaoywloveyou@gmail.com>
2018-07-20 20:43:53 -0400

    plot results

--- src/epa_expansion/compare.py
+++ src/epa_expansion/compare.py
@@ -1,21 +1,13 @@
 import json
 import numpy as np
+from nltk.corpus import wordnet as wn
 from sample_seeds import read_warriner_ratings
 from collections import OrderedDict
 
 tokens = ['male', 'female', 'adult', 'mom', 'father']
 
 
-if __name__ == '__main__':
-    with open('../result/epa_expansion/nn_result_github_all', 'r') as fp:
-        github = json.load(fp)
-    with open('../result/epa_expansion/nn_result_twitter_all', 'r') as fp:
-        twitter = json.load(fp)
-    warriner = read_warriner_ratings()
-
-    for token in tokens:
-        print('%s %s %s %s' % (token, github[token], twitter[token], warriner[token]))
-
+def overall_comp():
     common_tokens = set(github.keys()) & set(twitter.keys())
     dis_e, dis_p, dis_a, dis_ave = {}, {}, {}, {}
     for token in common_tokens:
@@ -35,3 +27,50 @@ if __name__ == '__main__':
         json.dump(dis_a, fp)
     with open('../result/epa_expansion/comparison_ave', 'w') as fp:
         json.dump(dis_ave, fp)
+
+
+def get_tokenset(words_list):
+    words_list_pos = {
+        wn.VERB: [],
+        wn.NOUN: [],
+        wn.ADV: [],
+        wn.ADJ: []
+    }
+
+    for word in words_list:
+        for pos in words_list_pos.keys():
+            if word in set(w.name().split('.', 1)[0] for w in wn.synsets(word, pos=pos)):
+                words_list_pos[pos].append(word)
+
+    return words_list_pos
+
+
+def tokenset_comp():
+    words_list_pos = get_tokenset(set(github.keys()) & set(twitter.keys()))
+    for pos in words_list_pos.keys():
+        tokenlist = words_list_pos[pos]
+        github_list = np.array([github[t] for t in tokenlist])
+        twitter_list = np.array([twitter[t] for t in tokenlist])
+        print('===== %s =====' % pos)
+        print(np.mean(github_list, axis=0))
+        print(np.mean(np.abs(github_list), axis=0))
+        print(np.std(github_list, axis=0))
+
+        print(np.mean(twitter_list, axis=0))
+        print(np.mean(np.abs(twitter_list), axis=0))
+        print(np.std(twitter_list, axis=0))
+
+
+if __name__ == '__main__':
+    with open('../result/epa_expansion/nn_result_github_all', 'r') as fp:
+        github = json.load(fp)
+    with open('../result/epa_expansion/nn_result_twitter_all', 'r') as fp:
+        twitter = json.load(fp)
+    warriner = read_warriner_ratings()
+
+    # for token in tokens:
+    #     print('%s %s %s %s' % (token, github[token], twitter[token], warriner[token]))
+
+    tokenset_comp()
+
+
