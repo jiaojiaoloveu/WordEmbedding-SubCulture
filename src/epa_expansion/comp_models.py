4808a1b52ac8150c232a467f8fbb1b68b17114c6
Yuwei Jiao <jiaoywloveyou@gmail.com>
2018-08-09 16:45:24 -0400

    cmp token list

--- src/epa_expansion/comp_models.py
+++ src/epa_expansion/comp_models.py
@@ -18,7 +18,7 @@ def align(source, target, method):
         return np.matmul(source.vectors, w)
 
 
-if __name__ == '__main__':
+def main():
     gg_model = KeyedVectors.load_word2vec_format('../models/embedding/GoogleNews-vectors-negative300.bin', binary=True)
     model_name = 'word2vec_sg_0_size_300_mincount_20'
     gh_model = Word2Vec.load('../models/embedding/github/%s' % model_name)
@@ -40,3 +40,27 @@ if __name__ == '__main__':
     model_path = '../models/embedding/github_aligned'
     os.makedirs(model_path, exist_ok=True)
     gh_model_new.save(os.path.join(model_path, model_name))
+    del gg_model
+    del gh_model
+    del gh_model_new
+
+
+def cmp():
+    gg_model = KeyedVectors.load_word2vec_format('../models/embedding/GoogleNews-vectors-negative300.bin', binary=True)
+    model_name = 'word2vec_sg_0_size_300_mincount_20'
+    gh_model = Word2Vec.load('../models/embedding/github_aligned/%s' % model_name)
+    tokens = ['bug', 'debug', 'commit', 'push', 'pull', 'thread', 'crash', 'fix', 'git', 'merge', 'password',
+              'happy', 'glad', 'sad', 'sorry', 'regret', 'reject', 'angry', 'proud', 'cautious', 'excited',
+              'man', 'woman', 'male', 'female', 'boy', 'girl', 'programmer', 'sex', 'gay', 'civic']
+    for w in tokens:
+        print('==========')
+        print(w)
+        print(spatial.distance.cosine(gg_model[w], gh_model.wv[w]))
+        print('google')
+        print(gg_model.most_similar(w, topn=20))
+        print('github')
+        print(gh_model.wv.most_similar(w, topn=20))
+
+
+if __name__ == '__main__':
+    cmp()
